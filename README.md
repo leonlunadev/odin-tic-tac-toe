f83c16578d8ef26d7cc563582588585aa32282e1
Roberto Leon-Luna <leonlunadev@gmail.com>
2025-02-16 11:20:03 -0500

    adds gameBoard IIFE with setValue, getValue...

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 TIC TAC TOE
 
 2 PLAYERS
+player x and player o
+calls the game board with spot selection
 
 GAME CONTROL FLOW
+checks game board
+to see if game is won
+resets game board
 
 GAME BOARD
+Array
+Functions that fill the array with x and os
+checks if spot is occupied or not
+function to show what board looks like
+
+display controller
--- script.js
+++ script.js
@@ -1,5 +1,40 @@
+const gameBoard = (function () {
+  const gameArray = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
 
+  const resetGameBoard = () => {
+    for (let i = 0; i < gameArray.length; i++) {
+      gameArray[i].fill(""); // fills each row with empty strings
+    }
+  };
 
+  const setValue = (x, y, value) => (gameArray[x][y] = value);
 
+  const viewGameBoard = () => {
+    console.log("\n");
+    console.log(gameArray[0]);
+    console.log(gameArray[1]);
+    console.log(gameArray[2]);
+  };
 
+  const getValue = (x, y) => gameArray[x][y];
 
+  return {
+    resetGameBoard,
+    setValue,
+    getValue,
+    viewGameBoard,
+  };
+})();
+
+// Test if the script is running
+console.log("Script is running!");
+gameBoard.viewGameBoard(); // This will show the initial empty board
+
+gameBoard.setValue(0, 0, "x");
+gameBoard.viewGameBoard();
+
+console.log(gameBoard.getValue(0, 0));
