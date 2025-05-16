2fd1a439176df609c82f6408536e066a1c79254f
Roberto Leon-Luna <leonlunadev@gmail.com>
2025-05-16 13:26:45 -0400

    changes some background colors

--- script.js
+++ script.js
@@ -123,14 +123,14 @@ gameBoard.resetGameBoard();
 
 const createO = () => {
   return `<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="30" stroke="red" stroke-width="8" fill="none" stroke-linecap="round"/>
+        <circle cx="50" cy="50" r="30" stroke="#00ffcc" stroke-width="8" fill="none" stroke-linecap="round"/>
     </svg>`;
 };
 
 const createX = () => {
   return `<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <line x1="20" y1="20" x2="80" y2="80" stroke="black" stroke-width="8" stroke-linecap="round"/>
-      <line x1="80" y1="20" x2="20" y2="80" stroke="black" stroke-width="8" stroke-linecap="round"/>
+      <line x1="20" y1="20" x2="80" y2="80" stroke="#00ffcc" stroke-width="8" stroke-linecap="round"/>
+      <line x1="80" y1="20" x2="20" y2="80" stroke="#00ffcc" stroke-width="8" stroke-linecap="round"/>
   </svg>`;
 };
 
@@ -156,9 +156,9 @@ const clicked = (e) => {
     }
     turn += 1;
   } else {
-    squareElement.style.backgroundColor = "orange";
+    squareElement.style.backgroundColor = "#ff073a";
     setTimeout(() => {
-      squareElement.style.backgroundColor = "white";
+      squareElement.style.backgroundColor = "#ffffff";
     }, 250);
   }
 };
